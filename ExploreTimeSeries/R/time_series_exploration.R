a8373bdc72d735f0a85526d28807018471ee3ec9
Emanuele Fabbiani <donlelef@gmail.com>
2019-07-06 20:27:45 +0200

    Improve package structure

--- ExploreTimeSeries/R/time_series_exploration.R
+++ ExploreTimeSeries/R/time_series_exploration.R
@@ -1,92 +1,83 @@
+#' timeSeriesExploration
+#' A plugin to make time series exploration easier
+#'
+#' @export
 timeSeriesExploration <- function() {
-  library(dplyr)
-  library(lubridate)
-  library(miniUI)
-  library(shiny)
-  library(forecast)
-  library(plotly)
+  ui <- miniUI::miniPage(
+    miniUI::gadgetTitleBar("Time Series Visualization"),
 
+    miniUI::miniTabstripPanel(
+      miniUI::miniTabPanel("Data",
+        icon = shiny::icon("table"),
 
-
-
-  ui <- miniPage(
-    gadgetTitleBar("Time Series Visualization"),
-
-    miniTabstripPanel(
-      miniTabPanel("Data",
-        icon = icon("table"),
-
-        miniContentPanel(
+        miniUI::miniContentPanel(
           stableColumnLayout(
-            selectInput(
+            shiny::selectInput(
               label = "Select your dataset:",
               inputId = "dataset",
               choices = c(search_df())
             )
           ),
-          dataTableOutput("table")
+          shiny::dataTableOutput("table")
         )
       ),
 
-      miniTabPanel("Line plot",
-        icon = icon("chart-line"),
+      miniUI::miniTabPanel("Line plot",
+        icon = shiny::icon("chart-line"),
 
 
-        miniContentPanel(
-          selectInput(
+        miniUI::miniContentPanel(
+          shiny::selectInput(
             label = "Select time variable",
             inputId = "time_column",
             choices = NULL
           ),
-          selectizeInput(
+          shiny::selectizeInput(
             label = "Select your variables:",
             inputId = "line_columns",
             choices = NULL,
             multiple = TRUE
           ),
-
-
-
-          miniContentPanel(plotlyOutput("line_plot"))
+          miniUI::miniContentPanel(plotly::plotlyOutput("line_plot"))
         )
       ),
 
-      miniTabPanel("ScatterPlot",
-        icon = icon("braille"),
-        miniContentPanel(
-          selectInput(
+      miniUI::miniTabPanel("ScatterPlot",
+        icon = shiny::icon("braille"),
+        miniUI::miniContentPanel(
+          shiny::selectInput(
             label = "Select x variable",
             inputId = "x_variable",
             choices = NULL
           ),
-          selectizeInput(
+          shiny::selectizeInput(
             label = "Select y variables:",
             inputId = "y_variables",
             choices = NULL,
             multiple = TRUE
           ),
-
-
-
-          miniContentPanel(plotlyOutput("scatter_plot"))
+          miniUI::miniContentPanel(plotly::plotlyOutput("scatter_plot"))
         )
       ),
 
 
 
-      miniTabPanel("Correlogram",
-        icon = icon("bar-chart"),
+      miniUI::miniTabPanel("Correlogram",
+        icon = shiny::icon("bar-chart"),
 
         fillCol(
-          miniContentPanel(
-            selectInput(
+          miniUI::miniContentPanel(
+            shiny::selectInput(
               label = "select your variable",
               inputId = "corr_variable",
               choices = NULL
             ),
 
-            sliderInput(inputId = "lag", label = "Lag variable", min = 12, max = 48, value = 24),
-            fillRow(miniContentPanel(plotlyOutput("Correlogram")), miniContentPanel(plotlyOutput("PartialCorrelogram")))
+            shiny::sliderInput(inputId = "lag", label = "Lag variable", min = 12, max = 48, value = 24),
+            fillRow(
+              miniUI::miniContentPanel(plotly::plotlyOutput("Correlogram")),
+              miniUI::miniContentPanel(plotly::plotlyOutput("PartialCorrelogram"))
+            )
           )
         )
       )
@@ -94,29 +85,26 @@ timeSeriesExploration <- function() {
   )
 
 
-
-
-
   server <- function(input, output, session) {
-    data <- reactive({
+    data <- shiny::reactive({
       get(input$dataset, envir = .GlobalEnv)
     })
 
 
-    time_columns <- reactive({
+    time_columns <- shiny::reactive({
       data() %>%
-        select_if(is.Date) %>%
+        dplyr::select_if(is.Date) %>%
         colnames()
     })
 
-    numeric_columns <- reactive({
+    numeric_columns <- shiny::reactive({
       data() %>%
-        select_if(is.numeric) %>%
+        dplyr::select_if(is.numeric) %>%
         colnames()
     })
 
-    observe({
-      updateSelectInput(
+    shiny::observe({
+      shiny::updateSelectInput(
         session = session,
         inputId = "corr_variable",
         choices = numeric_columns(),
@@ -124,8 +112,8 @@ timeSeriesExploration <- function() {
       )
     })
 
-    observe({
-      updateSelectizeInput(
+    shiny::observe({
+      shiny::updateSelectizeInput(
         session = session,
         inputId = "line_columns",
         choices = numeric_columns(),
@@ -133,8 +121,8 @@ timeSeriesExploration <- function() {
       )
     })
 
-    observe({
-      updateSelectInput(
+    shiny::observe({
+      shiny::updateSelectInput(
         session = session,
         inputId = "time_column",
         choices = time_columns(),
@@ -143,8 +131,8 @@ timeSeriesExploration <- function() {
     })
 
 
-    observe({
-      updateSelectInput(
+    shiny::observe({
+      shiny::updateSelectInput(
         session = session,
         inputId = "x_variable",
         choices = numeric_columns(),
@@ -153,8 +141,8 @@ timeSeriesExploration <- function() {
     })
 
 
-    observe({
-      updateSelectizeInput(
+    shiny::observe({
+      shiny::updateSelectizeInput(
         session = session,
         inputId = "y_variables",
         choices = numeric_columns(),
@@ -162,40 +150,46 @@ timeSeriesExploration <- function() {
       )
     })
 
-    output$Correlogram <- renderPlotly({
+    output$Correlogram <- plotly::renderPlotly({
       req(input$corr_variable, input$lag)
 
-      autocorr <- ggAcf(data()[, input$corr_variable], lag.max = input$lag, type = "correlation")
-      ACF_chart <- ggplotly(autocorr + ggtitle("Autocorrelation function (ACF)"))
+      autocorr <- forecast::ggAcf(data()[, input$corr_variable], lag.max = input$lag, type = "correlation") +
+        ggplot2::ggtitle("Autocorrelation function (ACF)")
+      ACF_chart <- plotly::ggplotly(autocorr)
       ACF_chart
     })
-    #
-    #
-    output$PartialCorrelogram <- renderPlotly({
+
+    output$PartialCorrelogram <- plotly::renderPlotly({
       req(input$corr_variable, input$lag)
 
-      parautocorr <- ggPacf(data()[, input$corr_variable], lag.max = input$lag)
-      PACF_chart <- ggplotly(parautocorr + ggtitle("Partial Autocorrelation function (PACF)"))
+      parautocorr <- forecast::ggPacf(data()[, input$corr_variable], lag.max = input$lag) +
+        ggplot2::ggtitle("Partial Autocorrelation function (PACF)")
+      PACF_chart <- plotly::ggplotly(parautocorr)
       PACF_chart
     })
 
-
-    output$line_plot <- renderPlotly({
+    output$line_plot <- plotly::renderPlotly({
       req(input$time_column, input$line_columns)
 
 
-      chart <- plot_ly(data(), x = ~ get(input$time_column), y = ~ get(input$line_columns[1]), type = "scatter", mode = "lines", name = input$line_columns[1])
+      chart <- plotly::plot_ly(data(),
+        x = ~ get(input$time_column),
+        y = ~ get(input$line_columns[1]),
+        type = "scatter",
+        mode = "lines",
+        name = input$line_columns[1]
+      )
 
       if (length(input$line_columns) > 1) {
         more_cols <- input$line_columns[2:length(input$line_columns)]
         for (c in more_cols) {
           chart <- chart %>%
-            add_lines(y = data()[[c]], type = "scatter", mode = "lines", name = c)
+            plotly::add_lines(y = data()[[c]], type = "scatter", mode = "lines", name = c)
         }
       }
 
       chart %>%
-        layout(
+        plotly::layout(
           xaxis = list(title = "Data"),
           yaxis = list(title = "Value"),
           legend = list(orientation = "h", xanchor = "center", yanchor = "top", x = 0.5, y = 1.2)
@@ -204,22 +198,35 @@ timeSeriesExploration <- function() {
     })
 
 
-    output$scatter_plot <- renderPlotly({
+    output$scatter_plot <- plotly::renderPlotly({
       req(input$x_variable, input$y_variables)
 
-      chart <- plot_ly(data(), x = ~ get(input$x_variable), y = ~ get(input$y_variables[1]), type = "scatter", mode = "markers", name = input$y_variables[1]) %>%
-        add_trace(y = ~ get(input$x_variable), type = "scatter", mode = "lines", line = list(color = "black"), name = input$x_variable)
+      chart <- plotly::plot_ly(
+        data(),
+        x = ~ get(input$x_variable),
+        y = ~ get(input$y_variables[1]),
+        type = "scatter",
+        mode = "markers",
+        name = input$y_variables[1]
+      ) %>%
+        plotly::add_trace(
+          y = ~ get(input$x_variable),
+          type = "scatter",
+          mode = "lines",
+          line = list(color = "black"),
+          name = input$x_variable
+        )
 
       if (length(input$y_variables) > 1) {
         more_ys <- input$y_variables[2:length(input$y_variables)]
         for (c in more_ys) {
           chart <- chart %>%
-            add_trace(y = ~ get(c), type = "scatter", mode = "markers", name = c)
+            plotly::add_trace(y = ~ get(c), type = "scatter", mode = "markers", name = c)
         }
       }
 
       chart %>%
-        layout(
+        plotly::layout(
           xaxis = list(title = "X"),
           yaxis = list(title = "Y"),
           legend = list(orientation = "h", xanchor = "center", yanchor = "top", x = 0.5, y = 1.2)
@@ -228,17 +235,15 @@ timeSeriesExploration <- function() {
     })
 
 
-    output$table <- renderDataTable({
+    output$table <- shiny::renderDataTable({
       data <- data()
       data
     })
 
-    observeEvent(input$done, {
-      stopApp()
+    shiny::observeEvent(input$done, {
+      shiny::stopApp()
     })
   }
-
-
 
 
   viewer <- dialogViewer(
@@ -246,5 +251,5 @@ timeSeriesExploration <- function() {
     height = 1000,
     width = 1600
   )
-  runGadget(ui, server, viewer = viewer)
+  shiny::runGadget(ui, server, viewer = viewer)
 }
